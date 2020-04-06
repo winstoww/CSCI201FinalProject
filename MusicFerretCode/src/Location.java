6a107309eaf211573e69f55a56dc2cdaeca2d4d6
Arjun Mitra <mitraarj@usc.edu>
2020-04-06 15:39:04 +0530

    Added the profile and Location class
    Updated the profile class to store all the variable we want for the survey and added a Location class to store the latitude and longitude values

---
+++ MusicFerretCode/src/Location.java
@@ -0,0 +1,20 @@
+
+public class Location {
+	private Double latitude;
+	private Double longitude;
+	
+	public Location(Double latitude, Double longitude)
+	{
+		this.latitude=latitude;
+		this.longitude=longitude;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+	
+}
--- MusicFerretCode/src/Profile.java
+++ MusicFerretCode/src/Profile.java
@@ -1,4 +1,57 @@
+import java.util.ArrayList;
 
 public class Profile {
+	private String name;
+	private String password;
+	private String email;
+	private Location Location;
+	private ArrayList<String> instruments;
+	private ArrayList<String> genres;
+	private ArrayList<Integer> skill;
+	private boolean isPermanent;
+	
+	public Profile(String name,String password, String email, Location Location,ArrayList<String> instruments,
+			ArrayList<String> genres,ArrayList<Integer> skill, boolean isPermanent)
+	{
+		this.name=name;
+		this.password=password;
+		this.email= email;
+		this.Location= Location;
+		this.instruments=instruments;
+		this.genres=genres;
+		this.skill=skill;
+		this.isPermanent=isPermanent;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Location getLocation() {
+		return Location;
+	}
+
+	public ArrayList<String> getInstruments() {
+		return instruments;
+	}
+
+	public ArrayList<String> getGenres() {
+		return genres;
+	}
+
+	public ArrayList<Integer> getSkill() {
+		return skill;
+	}
+
+	public boolean isPermanent() {
+		return isPermanent;
+	}
 }
