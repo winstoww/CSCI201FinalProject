e58097a99ecae0ffef0f4c5e226387ad891dfdd0
Yong Shin <Yongeshi@usc.edu>
2020-04-11 20:11:03 -0400

    Create UserDatabase.sql

---
+++ MusicFerretCode/src/UserDatabase.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS UserInfo;
+CREATE DATABASE UserInfo;
+
+USE UserInfo;
+
+CREATE TABLE User (
+    ID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL
+);
+
+INSERT INTO User (name, email, password)
+	VALUES	('Yong', 'yongeshi@usc.edu', 'mypass');
