fa36f7961246e9a01923bccf0e2ca0118ce8b702
Yong Shin <Yongeshi@usc.edu>
2020-04-15 17:21:22 -0400

    Update LoginServlet.java

--- MusicFerretCode/src/LoginServlet.java
+++ MusicFerretCode/src/LoginServlet.java
@@ -5,8 +5,8 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
  
-@WebServlet("/UserLoginServlet")
-public class UserLoginServlet extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
  
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
