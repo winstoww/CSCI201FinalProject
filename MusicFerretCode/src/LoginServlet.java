fbdb7e51fd17b40602b8064ea8262717d1091a69
Yong Shin <Yongeshi@usc.edu>
2020-04-15 17:18:19 -0400

    Update LoginServlet.java

--- MusicFerretCode/src/LoginServlet.java
+++ MusicFerretCode/src/LoginServlet.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
  
 @WebServlet("/UserLoginServlet")
 public class UserLoginServlet extends HttpServlet {
