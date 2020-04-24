cebb8635092ffd2148925b7deaee5acc996c1bcb
Yong Shin <Yongeshi@usc.edu>
2020-04-24 12:42:27 -0400

    Update UserLoginServlet.java
    Updated to check if boxes are empty

--- MusicFerretCode/src/UserLoginServlet.java
+++ MusicFerretCode/src/UserLoginServlet.java
@@ -12,25 +12,29 @@ public class UserLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        DBUserLogin userLogin = new DBUserLogin();
-         
-        try {
-            Profile user = userLogin.tryLogin(email, password);
-            String destPage = "login.html";
-             
-            if (user == null) {
-                request.setAttribute("message", "Invalid email/password");
-            } else { 
-                HttpSession session = request.getSession(); //HttpSession is used to store the user's information until logged out
-                session.setAttribute("user", user); //***** TODO: use later when you need to access user information -> Profile user = session.getAttribute("user");
-                destPage = "dashboard.html";
-            }
-             
-            RequestDispatcher dispatch = request.getRequestDispatcher(destPage);
-            dispatch.forward(request, response);
-            
-        } catch (SQLException | ClassNotFoundException ex) {
-            throw new ServletException(ex);
+        String destPage = "login.html";
+        
+        if (email == null || password == null || email.length() == 0 || password.length() == 0) {
+            request.setAttribute("login_message", "Empty email/password");
         }
+        else {
+            try {
+                Profile user = DBUserLogin.tryLogin(email, password);
+
+                if (user == null) {
+                    request.setAttribute("login_message", "Invalid email/password");
+                } else { 
+                    HttpSession session = request.getSession(); //HttpSession is used to store the user's information until logged out
+                    session.setAttribute("user", user); //***** TODO: use later when you need to access user information -> Profile user = session.getAttribute("user");
+                    destPage = "dashboard.html";
+                }
+
+            } catch (SQLException | ClassNotFoundException ex) {
+                throw new ServletException(ex);
+            }
+        }
+     
+        RequestDispatcher dispatch = request.getRequestDispatcher(destPage);
+        dispatch.forward(request, response);
     }
 }
